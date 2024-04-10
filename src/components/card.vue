2be4fdd51837efa3632f15e8e645eda191335932
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-04-10 16:05:56 +0200

    add form

---
+++ src/components/card.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "singlecard",
+  props: {
+    item: Object,
+  },
+};
+</script>
+
+<template>
+  <!-- {{ item.prova }} -->
+</template>
+
+<style scoped></style>
--- src/components/cards.vue
+++ src/components/cards.vue
@@ -1,11 +1,50 @@
 <script>
+import singlecard from "./card.vue";
 export default {
+  components: {
+    singlecard,
+  },
   name: "cards",
+
+  data() {
+    return {
+      array: [
+        {
+          prova: 1,
+        },
+        {
+          prova: 1,
+        },
+        {
+          prova: 1,
+        },
+      ],
+      ricerca: "",
+      repositories: "",
+    };
+  },
+  methods: {
+    getElements() {
+      console.log(this.ricerca);
+      console.log(this.repositories);
+    },
+  },
 };
 </script>
 
 <template>
-  <div class="text-center">ciao</div>
+  <form @submit.prevent="getElements">
+    <input type="text" v-model="ricerca" />
+    <select name="" id="" v-model="repositories">
+      <option value="repositories" selected>repositories</option>
+      <option value=">user">user</option>
+    </select>
+    <input type="submit" value="cerca" />
+  </form>
+
+  <div v-for="number in array">
+    <singlecard :item="number" />
+  </div>
 </template>
 
 <style scoped></style>
