97f2477d418b6e3850b74c85d509a986af0b91d5
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-04-10 15:40:43 +0200

    set-up progetto

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import cards from "./components/cards.vue";
+
+export default {
+  components: {
+    cards,
+  },
+};
+</script>
+
+<template>
+  <cards />
+</template>
+
+<style scoped lang="scss">
+@import "./style/general.scss";
+</style>
---
+++ src/components/cards.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  name: "cards",
+};
+</script>
+
+<template>
+  <div class="text-center">ciao</div>
+</template>
+
+<style scoped></style>
